480fc89d9510d923b1d44251016bb491db11b740
League Student <league@iMac-24.attlocal.net>
2019-12-24 19:56:51 -0800

    12/24/19
    12/24/19

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -3,25 +3,32 @@ package arrays;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] names = {"John", "Joseph", "James", "Julian", "Jeremy"};
 		//2. print the third element in the array
-		
+		System.out.println(names[2]);
 		//3. set the third element to a different value
-		
+		names[2] = "Jacob";
 		//4. print the third element again
-		
+		System.out.println(names[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for (int i = 0; i < 5; i++) {
+			System.out.println(names[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+		int[] integers = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
-		
+		for (int i = 0; i < 50; i++) {
+			integers[i] = i;
+		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		System.out.println(integers[0]);
 		//9 print the entire array to see if step 8 was correct
-		
+		for (int i = 0; i < integers.length; i++) {
+			System.out.println(integers[i]);
+		}
 		//10. print the largest number in the array.
+		System.out.println(integers[49]);
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,17 +1,44 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
+	static boolean won = false;
 	//1. make a main method
+public static void main(String[] args) {
+	
 
 	//2. create an array of 5 robots.
-
+Robot[] robots = new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+for (int i = 0; i < robots.length; i++) {
+	robots[i]=new Robot();
+}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-
+for (int i = 0; i < robots.length; i++) {
+	robots[i].setX(50 + 100*i);
+	robots[i].setY(500);
+}
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
-
+while(won == false) {
+for (int i = 0; i < robots.length; i++) {
+	robots[i].move(new Random().nextInt(50));
+	if (robots[i].getY()<50) {
+		won = true;
+		robots[i].sparkle();
+		break;
+	}
+	if (robots[i].getY()<=400) {
+		robots[i].turn(90);
+	}
+	if (robots[i].getX()-i*100==100) {
+		robots[i].turn(270);
+	}
+	}
+}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 	//7. declare that robot the winner and throw it a party!
@@ -19,4 +46,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
